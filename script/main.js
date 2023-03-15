c7a5f396a59ef318c488c47cc0d257ed91bbe2ad
Luander Silva <98993493+LuanderSilva@users.noreply.github.com>
2023-03-15 16:26:49 -0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msn = document.querySelector('div#msn')
+    var foto = document.querySelector('img#imagem')
+    var data = new Date()
+    // var hora = data.getHours()
+    var hora =  13
+
+    msn.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        foto.src = './imagem/Manha.svg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        foto.src = './imagem/Tarde.svg'
+        document.body.style.background = '#b9846f'
+    } else {
+        foto.src = './imagem/Noite.svg'
+        document.body.style.background = '#515154'
+    }
+}
